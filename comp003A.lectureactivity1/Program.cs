6b9680e631bc3af58c82364a6ee1bec193633d5b
isaacmtz2429 <isaacmartinez2429@gmail.com>
2025-01-15 21:00:07 -0800

    10 things

--- comp003A.lectureactivity1/Program.cs
+++ comp003A.lectureactivity1/Program.cs
@@ -13,7 +13,13 @@ namespace comp003A.lectureactivity1
         // main level
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // prompts user for input
+            Console.Write("Enter your name: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("hi " + username + ". My name is Isaac Martinez");
+            Console.WriteLine("\n\nHere are 10 things about me");
+            Console.WriteLine("hi");
         }
     }
 }
