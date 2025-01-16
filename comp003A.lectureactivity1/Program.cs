4e83dda08b312a97257771798a2f30a18457f342
isaacmtz2429 <isaacmartinez2429@gmail.com>
2025-01-15 20:17:11 -0800

    Made a change

--- comp003A.lectureactivity1/Program.cs
+++ comp003A.lectureactivity1/Program.cs
@@ -1,4 +1,10 @@
-﻿// namespace level
+﻿/*
+ Author: Isaac Martinez
+ Course: COMP-003A
+ Faculty: Jonathan Cruz
+ Purpose: Basic console app
+ */
+// namespace level
 namespace comp003A.lectureactivity1
 {
    // class level
